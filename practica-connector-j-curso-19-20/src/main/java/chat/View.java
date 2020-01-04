c67830295ee9b0f8e9f967a01e9d157c500f2ed7
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2020-01-04 20:17:03 +0100

    Connector front output ready

--- practica-connector-j-curso-19-20/src/main/java/chat/View.java
+++ practica-connector-j-curso-19-20/src/main/java/chat/View.java
@@ -2,7 +2,15 @@ package chat;
 
 import chat.model.ChatRoom;
 import chat.model.Message;
+import com.mysql.cj.protocol.x.MessageConstants;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.text.BoxView;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
@@ -11,6 +19,7 @@ import java.util.Scanner;
  * Implementa la interfaz gráfica basada en línea de comandos de la aplicación. Esta clase no
  * debe ser modificada.
  */
+/*
 public class View {
 
     private Controller controller;
@@ -181,5 +190,256 @@ public class View {
         controller.sendMessage(this.userId, chatRoomId, text);
 
         this.showChatRoomMenu(chatRoomId);
+    }
+
+
+}
+*/
+
+public class View {
+
+    private Controller controller;
+
+    private String username;
+    private long userId;
+    private JFrame frame;
+    private JPanel inputPanel;
+    private JPanel outputPanel;
+    private JTextArea textInput;
+    private JTextArea textOutput;
+    private JScrollPane scrollInput;
+    private JScrollPane scrollOutput;
+
+    public View () {
+        //output
+        outputPanel = new JPanel();
+        outputPanel.setBorder( new TitledBorder( new EtchedBorder(), "output" ) );
+
+        textOutput = new JTextArea(10, 25);
+        textOutput.setText("");
+        textOutput.setEditable(false);
+        textOutput.setLineWrap(true);
+
+        scrollOutput = new JScrollPane(textOutput);
+        scrollOutput.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        outputPanel.add(scrollOutput);
+
+        //input
+        inputPanel = new JPanel();
+        inputPanel.setBorder( new TitledBorder( new EtchedBorder(), "input" ) );
+
+        textInput = new JTextArea(5, 25);
+        textInput.setText("");
+        textInput.setLineWrap(true);
+
+        scrollInput = new JScrollPane(textInput);
+        scrollInput.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        inputPanel.add(scrollInput);
+
+
+        //frame
+        frame = new JFrame();
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+        });
+
+
+        JPanel container = new JPanel();
+        container.setLayout( new BoxLayout(container, BoxLayout.PAGE_AXIS));
+        container.add(outputPanel);
+        container.add(inputPanel);
+
+        frame.add(container);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void show() throws SQLException {
+        this.showLoginMenu();
+    }
+
+    private void showLoginMenu() throws SQLException {
+        textOutput.append("\nBienvenido a nuestro chat\nPor favor, ingresa tu nombre de usuario: ");
+
+        do {
+            Scanner s = new Scanner(System.in);
+            this.username = s.nextLine().trim();
+        } while (username.length() == 0);
+
+        this.controller = new Controller();
+
+        this.userId = controller.createUser(username);
+
+        this.showMainMenu();
+    }
+
+    private void showMainMenu() throws SQLException {
+
+        int option;
+
+        do {
+            textOutput.append("\n¡Hola " + username + "!\nElija una acción:\n [1] Acceder a una sala de chat\n [2] Crear una sala de chat\n [3] Salir");
+
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine().trim();
+
+            try {
+                option = Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                option = 0;
+            }
+        } while (option < 1 || option > 3);
+
+        switch (option) {
+            case 1:  this.showAccessChatRoomMenu();
+                break;
+
+            case 2:  this.showCreateChatRoomMenu();
+                break;
+
+            default: textOutput.append("\n¡Hasta pronto!");
+        }
+
+    }
+
+    private void showCreateChatRoomMenu() throws SQLException {
+        String name;
+
+        do {
+            textOutput.append("\nEscriba el nombre de la sala de chat:");
+
+            Scanner s = new Scanner(System.in);
+            name = s.nextLine().trim();
+        } while (name.length() == 0);
+
+        controller.createChatRoom(this.userId, name);
+
+        this.showMainMenu();
+    }
+
+    private void showAccessChatRoomMenu() throws SQLException {
+
+        int option;
+
+        List<ChatRoom> chatRooms = controller.getChatRooms();
+
+        do {
+            textOutput.append("\nElija la sala de chat:");
+
+            for (int i = 0; i < chatRooms.size(); i++) {
+                textOutput.append("\n [" + (i+1) + "] " + chatRooms.get(i).getName());
+            }
+
+            textOutput.append("\n [" + (chatRooms.size()+1) + "] Volver");
+
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine().trim();
+
+            try {
+                option = Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                option = 0;
+            }
+        } while (option < 0 || option > (chatRooms.size() + 1));
+
+        if (option == (chatRooms.size()+1)) {
+            this.showMainMenu();
+
+        } else {
+            ChatRoom chatRoom = chatRooms.get(option - 1);
+            long chatRoomId = chatRoom.getId();
+            this.showChatRoomMenu(chatRoomId);
+        }
+    }
+
+    private void showChatRoomMenu(long chatRoomId) throws SQLException {
+        int option;
+
+        do {
+            textOutput.append("\nElija una acción:\n [1] Leer mensajes\n [2] Enviar un mensaje\n [3] Volver");
+
+            Scanner s = new Scanner(System.in);
+            String line = s.nextLine().trim();
+
+            try {
+                option = Integer.parseInt(line);
+            } catch (NumberFormatException e) {
+                option = 0;
+            }
+        } while (option < 1 || option > 3);
+
+        switch (option) {
+            case 1:  this.showChatRoomMessages(chatRoomId);
+                break;
+
+            case 2:  this.showSendMessageMenu(chatRoomId);
+                break;
+
+            default: this.showMainMenu();
+        }
+    }
+
+    private void showChatRoomMessages(long chatRoomId) throws SQLException {
+        List<Message> messages = controller.getMessages(chatRoomId);
+
+        textOutput.append("\nMensajes de la sala de chat:");
+
+        if (messages.size() == 0) {
+            textOutput.append("\nNo se han encontrado mensajes en esta sala de chat");
+        } else {
+            for (Message message : messages) {
+                System.out.print("\n" + message.toString());
+            }
+        }
+
+        textOutput.append("\n\nPresione ENTER para continuar");
+
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
+
+        this.showChatRoomMenu(chatRoomId);
+    }
+
+    private void showSendMessageMenu(long chatRoomId) throws SQLException {
+
+        String text;
+
+        do {
+            textOutput.append("\nEscriba el contenido del mensaje:");
+
+            Scanner s = new Scanner(System.in);
+            text = s.nextLine().trim();
+        } while (text.length() == 0);
+
+        controller.sendMessage(this.userId, chatRoomId, text);
+
+        this.showChatRoomMenu(chatRoomId);
+    }
+
+
+}
+
+class AppFrame extends Frame {
+    public AppFrame(int size, String name) {
+        super(name);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+                System.exit(0);
+            }
+        });
+
+        setSize(size, size);
+    }
+}
+
+class AppCanvas extends Canvas {
+    public AppCanvas(){
+
     }
 }
